fb9e259bdaaf90c436b4905b9e5c56523d666835
ezelpc <ezequielpc496@gmail.com>
2023-06-19 01:55:59 -0600

    Modificacion Principal
    Se modifico el código principal

--- AnalizadorSintacticoFinal/Principal.java
+++ AnalizadorSintacticoFinal/Principal.java
@@ -55,6 +55,17 @@ public class Principal {
 
         Parser parser = new Parser(tokens);
         parser.parse(); 
+
+         GeneradorPostfija gpf = new GeneradorPostfija(tokens);
+        List<Token> postfija = gpf.convertir();
+
+        /*for(Token token : postfija){
+            System.out.println(token);
+        }*/
+
+        GeneradorAST gast = new GeneradorAST(postfija);
+        Arbol programa = gast.generarAST();
+        programa.recorrer();
     }
 
    
